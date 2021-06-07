ad9741b4a468ca21fe883f801a3a6c569e2719e4
prasanna <42783725+madmadusanka@users.noreply.github.com>
2021-06-07 12:01:34 +0530

    Initial project clone
    Initial project clone.

---
+++ Assessment.Test/Assessment.Test/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Assessment.Test.ViewModels;
+using Assessment.Test.Views;
+using Assessment.Test.Views.Base;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Assessment.Test
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Page page = Activator.CreateInstance(typeof(HomeView)) as Page;
+            page.BindingContext = DependencyService.Get<HomeViewModel>();
+            MainPage = page;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Assessment.Test/Assessment.Test/Services/Interfaces/IHttpRequestProviderService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment.Test.Services
+{
+    public interface IHttpRequestProviderService
+    {
+
+        /// <summary>
+        /// HTTP POST
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        Task<HttpResponseMessage> PostAsync(string url, object content);
+
+    }
+}
